feb045a673ddd04a4ddb92eac2562f0d72d28b5a
c-o-d-e <swapyuk@gmail.com>
2022-09-14 14:46:23 +0100

    type info in ProjectsCard

---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,16 @@
+<template>
+    {{ projectData.ProjectsArray }}
+</template>
+
+<script>
+import jsonData from "../../projects.json";
+export default {
+    data() {
+        return {
+            projectData: jsonData,
+        };
+    },
+};
+</script>
+
+<style></style>
