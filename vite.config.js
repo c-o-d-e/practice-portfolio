7a8d257d6d685f1bed8d4ef4ad1fd1fa585ee0df
c-o-d-e <swapyuk@gmail.com>
2022-09-15 22:55:43 +0100

    deployment shortcut

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    publicPath:
+        process.env.NODE_ENV === "production" ? "practice-portfolio" : "/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
