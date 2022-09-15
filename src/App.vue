69d606937c4c77125f09e6cd5cd7df56195bcdd0
c-o-d-e <swapyuk@gmail.com>
2022-09-15 12:13:35 +0100

    Vue logo removed

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,6 @@ import ProjectsCard from "./components/ProjectsCard.vue";
 </script>
 
 <template>
-    <header>
-        <img
-            alt="Vue logo"
-            class="logo"
-            src="./assets/logo.svg"
-            width="125"
-            height="125"
-        />
-    </header>
     <MainCard />
 
     <ProjectsCard />
