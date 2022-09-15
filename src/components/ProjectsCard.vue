0585200cbdc2390f4aada333655241d5ecf4e5b8
c-o-d-e <swapyuk@gmail.com>
2022-09-15 11:54:20 +0100

    logic for project details

--- src/components/ProjectsCard.vue
+++ src/components/ProjectsCard.vue
@@ -1,5 +1,15 @@
 <template>
-    {{ projectData.ProjectsArray }}
+    <div class="main-container">
+        <div
+            class="projects"
+            v-for="items in projectData.ProjectsArray"
+            v-bind:key="items"
+        >
+            <h2>{{ items.Title }}</h2>
+            <img :src="items.Image" alt="image" />
+            <p>{{ items.About }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
